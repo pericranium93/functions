f2581bfbda728dbf41dbfda614f24bf392d66f4f
konovalovdv <89204140266kon@gmail.com>
2021-01-24 19:48:45 +0300

    updated files task123.py & task5.py

--- task123.py
+++ task123.py
@@ -3,31 +3,40 @@ def my_f(x, y):
 
 
 result = None
-num1, num2 = int(input('Введите делимое и делитель')), int(input())
+num1, num2 = input('Введите делимое и делитель'), input()
 try:
+    num1, num2 = float(num1), float(num2)
     result = my_f(num1, num2)
+except ValueError:
+    print('Вы ввели символ!')
 except ZeroDivisionError:
     print('Деление на ноль невозможно!')
-print(result)
+
+print(f'{num1} / {num2} = {result}')
 
 
-################################
+###########Вторая задача#####################
+
+# Вариант 1
 def name_f(name, surname, birth, city, email, phone):
-    print(f'Данные пользователя: Имя: {name} Фамилия: {surname} Дата рождения: {birth} Город: {city} email: {email} '
-          f'Номер телефона: {phone}')
+    return (f'Данные пользователя: Имя: {name} Фамилия: {surname} Дата рождения: {birth} Город: {city} email: {email} '
+            f'Номер телефона: {phone}')
 
 
+# Вариант 2
 def name_kwargs(**user_data):
     print(f"Данные пользователя: {user_data}")
 
 
-name_f(name='Васлий', surname='Иванов', city='Гродно', phone=89204140266, email='dvsdvsd@mail.ru', birth='30.06.1985')
+print(name_f(name='Васлий', surname='Иванов', city='Гродно', phone=89204140266, email='dvsdvsd@mail.ru', birth='30.06'
+                                                                                                               '.1985'))
 
-name_kwargs(name="Васлий", surname="Иванов", birth="30.06.1985", city='Гродно', email='dvsdvsd@mail.ru',
-            phone='89204140266')
+print(name_kwargs(name="Васлий", surname="Иванов", birth="30.06.1985", city='Гродно', email='dvsdvsd@mail.ru',
+                  phone='89204140266'))
 
 
-##################################
+###########Третья задача#####################
+# Вариант 1
 def my_func(first, second, third):
     a = [third, second, first]
     a.sort()
@@ -35,6 +44,7 @@ def my_func(first, second, third):
     return num_max + num_max_2
 
 
+# Вариант 2
 def my_func_1(first, second, third):
     if first < second:
         first, second = second, first
--- task5.py
+++ task5.py
@@ -1,22 +1,30 @@
-amount = 0
-while True:
+def sum_elem(string):
+    """
+    Отфильтровывает значения по типу данных и возвращает сумму всех числовых значений списка.
+    :param string: Список, введенный пользователем
+    :return:
+    """
+    if 'q' in string:
+        last = string.index('q')
+        string = string[:last]
+        string = list(filter(lambda x: type(x) == float, string))
+        return sum(string)
+    else:
+        string = list(filter(lambda x: type(x) == float, string))
+        return sum(string)
+
+
+amount = 0.0
+finish = False
+while not finish:
     user_str = input('Введите вашу строку').split()
-    for element in user_str:
+    for i, element in enumerate(user_str):
         try:
-            if element == 'q':
-                break
+            user_str[i] = float(element)
         except ValueError:
             continue
+    amount += sum_elem(user_str)
+    print(f'Сумма всех числовых элементов = {amount}')
+    finish = True if 'q' in user_str else False  # Почему-то не позволяет использовать без else
 
 
-
-
-
-
-
-    # for element in user_str:
-    #     if element == 'q':
-    #         break
-    #     else:
-    #         amount += float(element)
-    # print(amount)
